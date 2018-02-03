337a9af432217dd9ee9189f1d615c2685f83b81c
gu <1376295429@qq.com>
2018-02-03 22:18:17 +0800

    第一天

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var sass = require('gulp-sass');
 
 gulp.task('compileSass',function(){
 	gulp.src('./src/sass/*.scss')
-	.pipe(sass({outputStyle:'expanded'}).on('error', sass.logError))
+	.pipe(sass({outputStyle:'compact'}))
 	.pipe(gulp.dest('./src/css/'))
 });
 
@@ -13,7 +13,7 @@ var browserSync = require('browser-sync');
 gulp.task('server',function(){
 	browserSync({
 		port:1008,
-		proxy:'http://localhost:17080',
+		proxy:'http://localhost:1000',
 		files:['./src/**/*.html','./src/css/*.css','./src/api/*.php']
 	});
 	gulp.watch('./src/sass/*.scss',['compileSass']);
